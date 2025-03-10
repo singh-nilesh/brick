a1a78d2032d261d25b7aef75f5a42bda058d050c
singh-nilesh <nileshsingh2021fybsc@gmail.com>
2025-03-10 09:04:50 +0530

    issue fix

--- firebaseConfig.tsx
+++ firebaseConfig.tsx
@@ -5,7 +5,6 @@ import { getStorage } from "firebase/storage";
 const firebaseConfig = {
  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
- databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
